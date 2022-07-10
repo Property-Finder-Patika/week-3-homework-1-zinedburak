5a55470d5ecb5ed9293e70de3f55adddc0a30237
zinedburak <zined_burak@hotmail.com>
2022-07-10 22:36:54 +0300

    Chapter 14-9,10,11,12,13 exercises final

--- inancgumusExamples/Chapter-14/11-average/main.go
+++ inancgumusExamples/Chapter-14/11-average/main.go
@@ -8,6 +8,12 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+	"strconv"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Find the Average
 //
@@ -40,4 +46,29 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+	args := os.Args[1:]
+	if l := len(args); l == 0 || l > 5 {
+		fmt.Println("Please tell me numbers (maximum 5 numbers).")
+		return
+	}
+
+	var (
+		sum   float64
+		nums  [5]float64
+		total float64
+	)
+
+	for i, v := range args {
+		n, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			continue
+		}
+
+		total++
+		nums[i] = n
+		sum += n
+	}
+
+	fmt.Println("Your numbers:", nums)
+	fmt.Println("Average:", sum/total)
 }
