dba04c1deda0b312813775999a7ac1821b1cd43e
zinedburak <zined_burak@hotmail.com>
2022-07-10 22:32:43 +0300

    Chapter 14-7 exercises final

--- inancgumusExamples/Chapter-14/07-assign/main.go
+++ inancgumusExamples/Chapter-14/07-assign/main.go
@@ -8,6 +8,11 @@
 
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 // ---------------------------------------------------------
 // EXERCISE: Assign the Arrays
 //
@@ -42,4 +47,16 @@ package main
 // ---------------------------------------------------------
 
 func main() {
+	books := [...]string{"Kafka's Revenge", "Stay Golden", "Everythingship"}
+
+	upper, lower := books, books
+
+	for i := range books {
+		upper[i] = strings.ToUpper(upper[i])
+		lower[i] = strings.ToLower(lower[i])
+	}
+
+	fmt.Printf("books: %q\n", books)
+	fmt.Printf("upper: %q\n", upper)
+	fmt.Printf("lower: %q\n", lower)
 }
